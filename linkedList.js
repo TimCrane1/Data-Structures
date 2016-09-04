d6fcba1634e562ab807461a4d1301ce35ee116ad
Timothy Crane <mickeymouth82@gmail.com>
2016-09-04 16:42:50 -0400

    singly linked-list w/ addToTail, removeHead, and contains methods

--- linkedList.js
+++ linkedList.js
@@ -1,80 +1,52 @@
-function List(){
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-}
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
 
-function Node(value){
-    this.value = value;
-    this.next = null;
+  //  addToTail time complexity is O(n) or linear-time
+  list.addToTail = function(value) {
+    if (list.head === null) {
+      list.head = new Node(value);
+      list.tail = list.head;
+    } else {
+      var temp = this.tail;
+      temp.next = new Node(value);
+      this.tail = temp.next;
+    }
+    console.log('list', list)
+  };
+
+  //  removeHead time complexity is 0(1) or constant-time
+  list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
+  };
+
+  //  contains time complexity is 0(n) or linear-time
+  list.contains = function(target) {
+    var current = list.head;
+    if (list.tail.value === target) {
+      return true;
+    }
+    while (current.next) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    };
+    return false;
+  };
+  return list; 
+  };
+
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
 };
 
-List.prototype.append = function(value){
-//if list is empty, head = new node, else, traverse list to last node, next = new node
-    var current = this.head;
-    if(this.length === 0){
-        this.head = new Node(value);
-    }
-    else{
-        while(current.next){
-            current = current.next;
-        }
-        current.next = new Node(value);
-    }
-    this.length++;
-}
-
-//TODO currently inserting one position too far ahead
-List.prototype.insert = function(value, index){
-    var current = this.head;
-    var previous;
-    if (index === 0) {
-        this.head = new Node(value);
-        if (this.length != 0) {
-            this.head.next = current;
-        }
-    }
-    else if (index >= this.length -1) {
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = new Node(value);
-    }
-    else {
-        previous = current;
-        for (var i = 0; i < this.length; i++) {
-            previous = current;
-            current = current.next;
-            if (i === index) {
-                previous.next = new Node(value);
-                previous.next.next = current;
-            }
-        }
-    }
-    this.length++;
-}
-
-List.prototype.contains = function(target){
-//traverse list if value found return true, else, return false
-    if (this.length === 0) return false;
-    var current = this.head;
-    for (var i = 0; i < this.length; i++) {
-        if (current.value === target) {
-            return true;
-        }
-        current = current.next;
-    }
-    return false;
-}
-
-List.prototype.remove = function(index){
-//traverse to the object, connect previous and next, delete node
-}
-
-List.prototype.length = function(){
-    return this.length;
-}
-
-List.prototype.indexOf = function(){
-    return /*index of first instance of value, else return -1 if not found*/
-}
